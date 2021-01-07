2c818389d2037b38364bc53404faed18a2371920
Mizux Seiha <mizux.dev@gmail.com>
2021-01-07 14:42:28 +0100

    Fix compilation

--- Match3/test/src/Matrix_UT.cpp
+++ Match3/test/src/Matrix_UT.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 #include <Match3/Matrix.hpp>
 using match3::Matrix;
--- Signal/test/src/Signal_UT.cpp
+++ Signal/test/src/Signal_UT.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 #include <Signal/Signal.hpp>
 
 namespace {
