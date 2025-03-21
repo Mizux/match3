9901b01d5a652d5ed425ae6127883b956128bf8e
Corentin Le Molgat <corentinl@google.com>
2025-03-21 14:07:35 +0100

    ci: fix sample

--- ci/sample/main.cpp
+++ ci/sample/main.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-#include <numeric>
-
 #include <Match3/Game.hpp>
+#include <cstdlib>
 
 using namespace match3;
 
 int main(int /*argc*/, char** /*argv*/) {
-	Game game;
-  game.resize({8, 8})
-	game.setTypes(Types({{"a"}, {"b"}, {"c"}}));
-	game.fillBoard();
+  Game game;
+  game.resize({8, 8});
+  game.setTypes(Types({{"a"}, {"b"}, {"c"}}));
+  game.fillBoard();
 
-	return 0;
+  return EXIT_SUCCESS;
 }
-
